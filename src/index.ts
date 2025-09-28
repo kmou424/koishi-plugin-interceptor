1c461e0da58465f3cc90272de3ca43ef53f15bcb
kmou424 <me@kmou424.moe>
2025-09-28 14:24:12 +0800

    fix: remove some unnessary event

--- src/index.ts
+++ src/index.ts
@@ -16,21 +16,6 @@ export function apply(ctx: Context, config: Config) {
   migrate(ctx);
 
   const events: (keyof Events<Context>)[] = [
-    "friend-request",
-    "guild-added",
-    "guild-member-added",
-    "guild-member-removed",
-    "guild-member-request",
-    "guild-member-updated",
-    "guild-removed",
-    "guild-request",
-    "guild-role-created",
-    "guild-role-deleted",
-    "guild-role-updated",
-    "guild-updated",
-    "login-added",
-    "login-removed",
-    "login-updated",
     "message",
     "message-deleted",
     "message-updated",
